57517fd7afffc784477e917997dd7b1c05abb10e
ajkinder <alexjohnkinder@gmail.com>
2021-03-22 22:28:52 -0700

    Added UI elements to SettingsView. Added SettingsLabelView

--- Obst/App/ContentView.swift
+++ Obst/App/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTIES
     
+    @State private var isShowingSettings: Bool = false
+    
     var fruits: [Fruit] = fruitsData
     
     // MARK: - BODY
@@ -18,12 +20,22 @@ struct ContentView: View {
             List {
                 ForEach(fruits.shuffled()) { item in
                     NavigationLink(destination: FruitDetailView(fruit: item)) {
-                    FruitRowView(fruit: item)
-                        .padding(.vertical, 4)
-                }
+                        FruitRowView(fruit: item)
+                            .padding(.vertical, 4)
+                    }
                 }
             } //: LIST
             .navigationTitle("Fruits")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        isShowingSettings = true
+                                    }) {
+                                        Image(systemName: "slider.horizontal.3")
+                                    } //: BUTTON
+                                    .sheet(isPresented: $isShowingSettings) {
+                                        SettingsView()
+                                    }
+            )
         } //: NAVIGATION
     }
 }
