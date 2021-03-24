19e28ffa402592612f78a0709a530427ffdcd204
ajkinder <alexjohnkinder@gmail.com>
2021-03-24 13:55:36 -0700

    Enhancements to iPad version of application

--- Obst/App/SettingsView.swift
+++ Obst/App/SettingsView.swift
@@ -46,10 +46,22 @@ struct SettingsView: View {
                             .font(.footnote)
                             .multilineTextAlignment(.leading)
                         
-                        // Toggle
+                        
                         Toggle(isOn: $isOnboarding) {
-                            Text("Restart".uppercased())
-                        }
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                        }//:TOGGLE
+                        .padding()
+                        .background(Color(UIColor.tertiarySystemBackground)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8,style: .continuous))
+                        )
                         
                     }//: GROUPBOX
                     
